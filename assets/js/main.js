4c3f215c7e9e7bc1e07347d144b3b2b3830ccb06
Paulina Gonzalez C <pgc.lacroix@gmail.com>
2017-07-18 14:16:19 -0400

    ejercicio

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+/*
+<i class="fa fa-microphone right" aria-hidden="true"></i>
+<i class="fa fa-ellipsis-v right" aria-hidden="true"></i>
+<i class="fa fa-paperclip right" aria-hidden="true"></i>
+<i class="fa fa-address-card" aria-hidden="true"></i>
+*/
+
+// gris #ebebeb 
